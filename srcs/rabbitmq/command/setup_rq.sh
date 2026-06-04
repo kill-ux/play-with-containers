9b8ade2e1e4d7ec130f9f7df7725eefe04dfff8e
kill-ux <mustaphaboutoubdev@gmail.com>
2026-06-04 13:22:29 +0100

    rabbit setup

--- srcs/rabbitmq/command/setup_rq.sh
+++ srcs/rabbitmq/command/setup_rq.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 set -euo pipefail
+echo $HOSTNAME
+rabbitmq-server &
+TEMP_PID=$!
+
+echo "Waiting for RabbitMQ to become fully responsive..."
+echo "TEMP_PID=> $TEMP_PID"
+rabbitmqctl wait --timeout 15 /var/lib/rabbitmq/mnesia/rabbit@$HOSTNAME.pid
 
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASS || true
 rabbitmqctl set_permissions -p / $RABBITMQ_USER ".*" ".*" ".*" || true
+
+service rabbitmq-server stop
+wait $TEMP_PID
+
+exec rabbitmq-server
